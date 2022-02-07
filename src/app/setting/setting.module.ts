9b2091a97e5785aba7fbfaf7e883bd2ece1af778
JiaJun831 <64371500+JiaJun831@users.noreply.github.com>
2022-02-07 16:19:49 +0000

    created home and setting

---
+++ src/app/setting/setting.module.ts
@@ -0,0 +1,20 @@
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SettingPage } from './setting.page';
+
+import { SettingPageRoutingModule } from './setting-routing.module';
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: SettingPage }]),
+    SettingPageRoutingModule,
+  ],
+  declarations: [SettingPage],
+})
+export class Tab3PageModule {}
